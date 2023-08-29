6268cc37458eaa1a4e3f394b0f4c9b66f803f74c
Manu2 <mbolanos50588@ufide.ac.cr>
2023-08-28 19:31:47 -0600

    Prueba
    Solamente probando

--- ProyectoIntroCajero/src/main/java/prfsemana03alejandroespinoza/proyectointrocajero/Main.java
+++ ProyectoIntroCajero/src/main/java/prfsemana03alejandroespinoza/proyectointrocajero/Main.java
@@ -11,7 +11,7 @@ package prfsemana03alejandroespinoza.proyectointrocajero;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Bienvenido!");
+        System.out.println("Bienvenido!.");
         clsVerificacion verificacion = new clsVerificacion();
         verificacion.ingresarDatos();
     }
