2f191c21e44cd46bb26e09b438ece6cdb9508cb2
AleEsp76 <56982261+AleEsp76@users.noreply.github.com>
2023-09-01 08:15:02 -0600

    Tercer Commit Manuel Bolaños
    Cambios y arrelgos a menu administradores

--- ProyectoIntroCajero/src/main/java/prfsemana03alejandroespinoza/proyectointrocajero/clsInicio.java
+++ ProyectoIntroCajero/src/main/java/prfsemana03alejandroespinoza/proyectointrocajero/clsInicio.java
@@ -161,5 +161,181 @@ public class clsInicio {
                     + "por favor contacte a un administrador para que esta sea desbloqueada nuevamente");
         }
     }
+    
+    public void MenuAdministrador(){
+        //Llamadas de clses
+        clsHelper clsH = new clsHelper();
 
+        //Opciones
+        char opcion = ' ';
+        
+        //Transacciones 
+        int dinero_a_introducir = 0;
+        int dinero_a_extraer = 0;
+        boolean permitido = false;
+        
+        do{
+            opcion = clsH.recibeChar("Seleccione una de las siguientes opciones:\n"
+                    + "A. Generar lista administradores\n"
+                    + "B. Agregar Administrador\n"
+                    + "C. Modificar Administrador\n"
+                    + "D. Habilitar/Deshabilitar Administrador\n"
+                    + "E. Listar Administradores\n"
+                    + "F  Administrar Clientes\n"
+                    + "G. Extraccion o Ingreso de dinero\n" 
+                    + "H. Reportes\n"
+                    + "S. Sair");   
+            switch (opcion) {
+                case 'A':
+                    if (listaAdministradores) {
+                        char nuevaLista = ' ';
+                        do {
+                            nuevaLista = clsH.recibeChar("¿Desea generar una nueva lista? \n S - Si \n N - No");
+                        } while (nuevaLista != 'S' && nuevaLista != 'N');
+                        if (nuevaLista == 'S') {
+                            administradores = clsAdmin.generarlistaAdministrador();
+                            posAdministrador = 0;
+                            listaAdministradores = true;
+                        }
+                    } else {
+                        clsH.imprimeMensaje("Se genera una lista de administradores nueva");
+                        administradores = clsAdmin.generarlistaAdministrador();
+                        clsH.imprimeMensaje("Lista de administradores generada");
+                        listaAdministradores = true;
+                    }
+                    break;
+                case 'B':
+                    clsH.imprimeMensaje("Se agrega un administrador");
+                    if (listaAdministradores) {
+                        if (posAdministrador < administradores.length) {
+                            posAdministrador = clsAdmin.agregarAdministrador(administradores, posAdministrador);
+                            clsH.imprimeMensaje("El administrador ha sido agregado exitosamente");
+                        } else {
+                            clsH.imprimeMensaje("La lista de administradores se ha llenado");
+                        }
+                    } else {
+                        clsH.imprimeMensaje("Lista de administradores aun no ha sido generada generada");
+                    }
+                    break;
+                case 'C':
+                    if (posAdministrador == 0) {
+                    clsH.imprimeMensaje("No existen administradores, agregue uno primero");
+                    } else {
+                    clsH.imprimeMensaje("Se modifica un administrador");
+                    administradores = clsAdmin.modificarAdministrador(administradores, posAdministrador);
+                    }
+                    break;
+                case 'D':
+                    if (posAdministrador == 0) {
+                        clsH.imprimeMensaje("No existen administradores, agregue uno primero");
+                    } else {
+                        clsAdmin.deshabilitarAdministrador(administradores, posAdministrador);
+                    //administradores = clsAdmin.cambiarEstadoHabilitadoDeshabilitado(administradores, posAdministrador);
+                    }
+                    break;
+                case 'E':
+                    clsH.imprimeMensaje("Se listan los administradores");
+                    if (listaAdministradores) {
+                        if (posAdministrador == 0) {
+                            clsH.imprimeMensaje("No hay clientes para mostrar en la lista");
+                        } else {
+                            clsAdmin.listarAdministradores(administradores, posAdministrador);
+                        }
+                    } else {
+                        clsH.imprimeMensaje("Aun no se ha generado una lista");
+                    }
+                    break;
+                case 'F':
+                    this.MenuAdministrarClientes();
+                    break;
+                case 'G':
+                    char opcion_ingreso_extraccion = ' ';
+                    do{
+                        opcion_ingreso_extraccion = clsH.recibeChar("Digite una de las siguientes opciones:\n"
+                                + "A)Dinero actual en colones\n"
+                                + "B)Dinero actual en denominaciones\n"
+                                + "C)Extraer dinero\n"
+                                + "D)Ingresar dinero\n"
+                                + "S)Salir");
+                        switch(opcion_ingreso_extraccion){
+                        case 'A':
+                            clsH.imprimeMensaje("El dinero actual en colones es de "+dineroMaquina+" colones");
+                        break;
+                        case 'B':
+                            clsAdmin.desglosar(dineroMaquina);
+                        break;
+                        
+                        case 'C':
+                            permitido = false;
+
+                                dinero_a_extraer = clsH.recibeInt("Digite la cantidad de dinero que desea extraer");
+                                if (dinero_a_extraer > dineroMaquina){
+                                    clsH.imprimeMensaje("La cantidad marcada supera los fondos disponibles por favor vuelva a ingresar una cantidad");
+                                }else if (dinero_a_extraer < 1000){
+                                    clsH.imprimeMensaje("La denominacion más baja es de 1000 colones");
+                                }else{
+                                    permitido  = true;
+                                }
+                            if (permitido){
+                                dineroMaquina -= dinero_a_extraer;
+                                transacciones_administradores++;
+                                transacciones_generales++;
+                                cantidad_extraida_administradores += dinero_a_extraer;
+                                clsAdmin.extraccionAdministrador(dinero_a_extraer);
+                                clsH.imprimeMensaje("Transaccion realizada con exito");
+                            }       
+                        break;
+                        
+                        case 'D':
+                            do{
+                                dinero_a_introducir = clsH.recibeInt("Digite la cantidad de dinero que desea introducir:");
+                            }while(dinero_a_extraer < 1000);
+                            dineroMaquina += dinero_a_introducir;
+                            transacciones_administradores++;
+                            transacciones_generales++;
+                            cantidad_ingresada_administradores += dinero_a_introducir;
+                            clsH.imprimeMensaje("Transaccion realizada con exito");
+                        break;
+                        
+                        case 'S':
+                        break;
+                        default:
+                            clsH.imprimeMensaje("Opcion invalida");
+                        break;
+                        }   
+                    }while(opcion_ingreso_extraccion != 'S');
+                    break;
+                    
+                case 'H':
+                    char opcion_reporte =' ';
+                    do{
+                        opcion_reporte = clsH.recibeChar("Digite una de las siguientes opciones:\n"
+                                + "A)Totalizado por fecha\n"
+                                + "B)Detallado por rango de fechas\n"
+                                + "S)Salir");
+                        switch(opcion_reporte){
+                            case 'A':
+                                //Imprime reporte totalizado
+                                break;
+                            case 'B':
+                                //Imprime reporte detallado
+                                break;
+                            case 'S':
+                                //Salir
+                                break;
+                            default:
+                                clsH.imprimeMensaje("Opcion invalida");
+                                break;
+                        }
+                        }while (opcion_reporte !='S');
+                    break;
+                case 'S':
+                    break;
+                default:
+                    clsH.imprimeMensaje("La opcion no es valida");
+                    break;
+            }
+            
+        }while(opcion != 'S');
+    }
 }
